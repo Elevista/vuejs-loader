ddd1b27b3c1cd86c4e772f637c3b4631edca27dc
Elevista <sunnyholic@sunnyholic.com>
2017-07-19 12:38:45 +0900

    2.0.0 wrapping vue-loader

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const esprima = require('esprima'), _ = require('lodash');
-module.exports = function (source) {
-	let module = esprima.parse(source, {sourceType: 'module'});
-	let properties = _.chain(module.body).find({type: 'ExportDefaultDeclaration'}).get('declaration.properties');   //export default statement
-	if (!properties.value()) properties = _.chain(module.body).find({
-		type: 'ExpressionStatement',
-		expression: {
-			left: {
-				object: {name: 'module'}, property: {name: 'exports'}	//module.exports statement
-			}
-		}
-	}).get('expression.right.properties');
-	let templateUrl = properties.find({key: {name: 'templateUrl'}}).get('value.value').value();
-	let styleUrl = properties.find({key: {name: 'styleUrl'}}).get('value.value').value();
-	let ret = `<script>\n${source}\n</script>`;
-	if (templateUrl) ret += `\n<template src="${templateUrl}"></template>`;
-	if (styleUrl) ret += `\n<style scoped src="${styleUrl}"></style>`;
-	return ret;
+const path = require('path'), loaderUtils = require('loader-utils');
+const vuejsLoaderPath = path.resolve(__dirname, './vuejs-loader.js');
+module.exports = function () {
+	let request = loaderUtils.getRemainingRequest(this);
+	let importString = loaderUtils.stringifyRequest(this, '!!vue-loader!' + vuejsLoaderPath + '!' + request);
+	return `import vueComponent from ${importString};\n`
+		+ 'export default vueComponent;';
 };
---
+++ vuejs-loader.js
@@ -0,0 +1,19 @@
+const esprima = require('esprima'), _ = require('lodash');
+module.exports = function (source) {
+	let module = esprima.parse(source, {sourceType: 'module'});
+	let properties = _.chain(module.body).find({type: 'ExportDefaultDeclaration'}).get('declaration.properties');   //export default statement
+	if (!properties.value()) properties = _.chain(module.body).find({
+		type: 'ExpressionStatement',
+		expression: {
+			left: {
+				object: {name: 'module'}, property: {name: 'exports'}	//module.exports statement
+			}
+		}
+	}).get('expression.right.properties');
+	let templateUrl = properties.find({key: {name: 'templateUrl'}}).get('value.value').value();
+	let styleUrl = properties.find({key: {name: 'styleUrl'}}).get('value.value').value();
+	let ret = `<script>\n${source}\n</script>`;
+	if (templateUrl) ret += `\n<template src="${templateUrl}"></template>`;
+	if (styleUrl) ret += `\n<style scoped src="${styleUrl}"></style>`;
+	return ret;
+};
