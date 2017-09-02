3346431386100afda7b445a51b3fd3cb1f00d250
Elevista <sunnyholic@sunnyholic.com>
2017-09-03 03:23:08 +0900

    eslint standard

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const path = require('path'), loaderUtils = require('loader-utils')
+const path = require('path')
+const loaderUtils = require('loader-utils')
 const vuejsLoaderPath = path.resolve(__dirname, './vuejs-loader.js')
 module.exports = function () {
   let request = loaderUtils.getRemainingRequest(this)
   let importString = loaderUtils.stringifyRequest(this, '!!vue-loader!' + vuejsLoaderPath + '!' + request)
-  return `import vueComponent from ${importString};\n`
-    + 'export default vueComponent;'
+  return `import vueComponent from ${importString};\n` +
+    'export default vueComponent;'
 }
--- vuejs-loader.js
+++ vuejs-loader.js
@@ -1,15 +1,18 @@
-const esprima = require('esprima'), _ = require('lodash')
+const esprima = require('esprima')
+const _ = require('lodash')
 module.exports = function (source) {
   let module = esprima.parse(source, {sourceType: 'module'})
-  let properties = _.chain(module.body).find({type: 'ExportDefaultDeclaration'}).get('declaration.properties')   //export default statement
-  if (!properties.value()) properties = _.chain(module.body).find({
-    type: 'ExpressionStatement',
-    expression: {
-      left: {
-        object: {name: 'module'}, property: {name: 'exports'}	//module.exports statement
+  let properties = _.chain(module.body).find({type: 'ExportDefaultDeclaration'}).get('declaration.properties')   // export default statement
+  if (!properties.value()) {
+    properties = _.chain(module.body).find({
+      type: 'ExpressionStatement',
+      expression: {
+        left: {
+          object: {name: 'module'}, property: {name: 'exports'}  // module.exports statement
+        }
       }
-    }
-  }).get('expression.right.properties')
+    }).get('expression.right.properties')
+  }
   let templateSrc = properties.find({key: {name: 'templateSrc'}}).get('value.value').value()
   let styleSrc = properties.find({key: {name: 'styleSrc'}}).get('value.value').value()
   let ret = `<script>\n${source}\n</script>`
